f6a82e289d0e12ce98254db50573a202c43c9c29
Scott <scott@scottbenton.dev>
2020-09-24 01:12:59 -0400

    Added head information - favicon, title, description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import "typeface-open-sans";
 import "typeface-playfair-display";
 import "typeface-fira-sans";
 import { Header, Content } from "../components/layout";
+import Head from "next/head";
 
 interface MyAppProps {
   Component: new () => React.Component<any>;
@@ -13,18 +14,25 @@ const MyApp: React.FC<MyAppProps> = (props) => {
   const { Component, pageProps } = props;
 
   return (
-    <div className={"cog-background min-h-screen"}>
-      <div
-        className={
-          "flex flex-col max-w-screen-lg mx-auto min-h-screen h-full bg-gray-100 shadow-2xl"
-        }
-      >
-        <Header />
-        <Content>
-          <Component {...pageProps} />
-        </Content>
+    <>
+      <Head>
+        <title>Eberron</title>
+        <meta name="description" content="Eberron Campaign Resources" />
+        <meta name="author" content="Scott Benton" />
+      </Head>
+      <div className={"cog-background min-h-screen"}>
+        <div
+          className={
+            "flex flex-col max-w-screen-lg mx-auto min-h-screen h-full bg-gray-100 shadow-2xl"
+          }
+        >
+          <Header />
+          <Content>
+            <Component {...pageProps} />
+          </Content>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
