a96859afa124a77db9c641f9bae4ec250103dc9e
Scott Benton <scott@scottbenton.dev>
2021-02-03 18:10:56 -0500

    Added Chronicle surrounding Avanth's death

--- content/chronicle/index.tsx
+++ content/chronicle/index.tsx
@@ -3,6 +3,7 @@ import * as Post2 from "./2020-09-30.mdx";
 import * as Post3 from "./2020-10-06.mdx";
 import * as Post4 from "./2020-10-12.mdx";
 import * as Post5 from "./2021-01-04.mdx";
+import * as Post6 from "./2021-02-03.mdx";
 
 export interface IChronicleEntry {
   meta: {
@@ -15,7 +16,7 @@ export interface IChronicleEntry {
   content: React.ReactNode;
 }
 
-const entries = [Post1, Post2, Post3, Post4, Post5];
+const entries = [Post1, Post2, Post3, Post4, Post5, Post6];
 
 export const chronicleEntries: IChronicleEntry[] = entries.map((Entry) => ({
   meta: Entry.meta,
