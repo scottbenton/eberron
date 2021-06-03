1d7f00acb7b0af5b422b60de6488f48a610f2a5e
Scott Benton <scott@scottbenton.dev>
2021-06-02 23:30:18 -0400

    Added padding for mobile devices

--- components/TextPage.tsx
+++ components/TextPage.tsx
@@ -1,7 +1,7 @@
 export const TextPage: React.FC = (props) => {
   const { children } = props;
   return (
-    <div className="max-w-xl block md:prose prose-sm mx-auto my-8 w-full flex-grow">
+    <div className="max-w-xl block md:prose prose-sm mx-auto my-8 w-full flex-grow px-8">
       {children}
     </div>
   );
